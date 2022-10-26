588d9dba3513a8c4569e60309ec323d3878e28b3
Sona Mariya Baby <sona.mb@pitsolutions.com>
2022-10-26 12:24:13 +0530

    updated

--- UserManagement.UI/wwwroot/js/user.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('.loginbtn').click(function () {
-        if ($('input#userName').val() === "1" && $('input#password').val() === "1") {
-            document.location.href = "https://localhost:44384/api/internal/users/" + userName + "/" + password;
-        } else {
-            alert('Incorrect Account Details');
-        }
-    });
-});
